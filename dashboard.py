8f4afdef5f7e097336060f885dc0d924aa517f20
unknown <glimadefreitasgit2002@gmail.com>
2025-07-09 16:05:03 -0300

    Instaciando o Dash

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,5 @@
+#%%
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -12,8 +14,35 @@ import json
 
 # Filtrando apenas os dados nescessarios 
 
-df = pd.read_csv("HIST_PAINEL_COVIDBR_13mai2021.csv", sep=';')
-df_states = df[~df["estado"].isna() & (df["codmun"].isna())]
-df_brasil = df[df["regiao"] == "Brasil"]
-df_states.to_csv("df_state.csv")
-df_brasil.to_csv("df_brasil.csv")
+# df = pd.read_csv("HIST_PAINEL_COVIDBR_13mai2021.csv", sep=';')
+# df_states = df[~df["estado"].isna() & (df["codmun"].isna())]
+# df_brasil = df[df["regiao"] == "Brasil"]
+# df_states.to_csv("df_states.csv")
+# df_brasil.to_csv("df_brasil.csv")
+
+
+df_states = pd.read_csv("df_states.csv")
+df_brasil = pd.read_csv("df_brasil.csv")
+
+df_states_ = df_states[df_states["data"] == "2020-05-13"]
+
+brazil_states = json.load(open("geojson/brazil_geo.json"))
+
+#=========================================
+# Instanciação do Dash
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
+
+fig = px.choropleth_mapbox(df_states, locations="estado", color="casosNovos",
+                           center={"lat": -16.95, "lon": -47.78},
+                           geojson=brazil_states, color_continuous_scale="Redor", opacity=0.4,
+                           hover_data={"casosAcumulado": True, "casosNovos": True, 
+                                       "obitosNovos": True, "estado": True})
+
+
+#=========================================
+# Layout
+
+
+
+# %%
