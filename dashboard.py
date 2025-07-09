ff1f0c06448941b792f750fe904a75c7d56c5015
unknown <glimadefreitasgit2002@gmail.com>
2025-07-09 13:41:03 -0300

    Iniciando o projeto Importando bibliotecas

---
+++ dashboard.py
@@ -0,0 +1,13 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, ClientsideFunction
+import dash_bootstrap_components as dbc
+import plotly.express as px
+import plotly.graph_objects as go
+
+import numpy as np
+import pandas as pd
+import json
+
+
